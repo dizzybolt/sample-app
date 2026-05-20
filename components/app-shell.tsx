b69339e196ec4dcf39af56ae917f255556e85e80
dizzybolt <dizzybolt@naver.com>
2026-05-20 21:50:56 +0900

    홈 화면 삭제 후 대시보드로 대체

--- components/app-shell.tsx
+++ components/app-shell.tsx
@@ -8,7 +8,6 @@ import {
   ClipboardList,
   FileCog,
   FileText,
-  Home,
   IdCard,
   Menu,
   PackageCheck,
@@ -22,7 +21,6 @@ import { cn } from '@/lib/utils'
 import { Button } from '@/components/ui/button'
 
 const workNavItems = [
-  { title: '홈', href: '/', icon: Home },
   { title: '대시보드', href: '/dashboard', icon: BarChart3 },
   { title: '샘플관리', href: '/samples', icon: ClipboardList },
   { title: '발주관리', href: '/orders', icon: FileText },
